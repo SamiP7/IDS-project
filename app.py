7216160b6863016ac949fb3d2a35d06e0d05407e
Karoliina Vainio <karoliina.vainio2@gmail.com>
2025-10-02 18:32:16 +0300

    Add app

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, g
+import sqlite3
+
+DB_PATH = "airbnb_london.db"
+app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DB_PATH)
+        db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
+
